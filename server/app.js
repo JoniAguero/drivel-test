54b02aad2d66ba28d65c932c898fb71390ab7802
Jonathan Aguero <jonathan@eelptex.com>
2021-01-11 17:42:35 +0100

    test API

---
+++ server/app.js
@@ -0,0 +1,32 @@
+
+const debug = require('debug')('back-end')
+const cors = require('cors')
+const logger = require('morgan')
+const express = require('express');
+const bodyParser = require('body-parser');
+// const phonesRoute = require('./routes/phones.route');
+// const authRoute = require('./routes/auth.route');
+
+require('dotenv').config();
+
+dbConnection();
+
+const app = express();
+app.use(logger('dev'))
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+// app.use('/api', [phonesRoute, authRoute]);
+
+
+app.use('/', (req, res) => {
+  res.send(`
+  <div style="width: 100%; text-align: center">
+    <h1 style >Hello :) , to make sure you use the API, use /api/phones </h1>
+  </div>`);
+});
+
+module.exports = app;
