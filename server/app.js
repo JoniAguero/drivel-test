e45288552f30a554fca0f1dc642ef06c6db3e004
Jonathan Aguero <jonathan@eelptex.com>
2021-01-11 17:49:37 +0100

    test API

--- server/app.js
+++ server/app.js
@@ -21,6 +21,33 @@ app.use(bodyParser.json());
 
 // app.use('/api', [phonesRoute, authRoute]);
 
+app.get("/api/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  } else {
+    db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new contact.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
+});
+
 
 app.use('/', (req, res) => {
   res.send(`
